dd14d60a62b6ba335accdb3e9da1b6ccdf8695b6
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-05-19 15:19:22 -0500

    add vowelCount logic

--- maps-sets.js
+++ maps-sets.js
@@ -1,1 +1,17 @@
 const hasDuplicate = arr => new Set(arr).size != arr.length;
+
+const vowelCount = (str) => {
+    const vowels = 'aeiou';
+    var vowelMap = new Map();
+    for (let letter of str) {
+        let lowerLetter = letter.toLowerCase();
+        if (vowels.includes(lowerLetter)) {
+            if (!vowelMap.has(lowerLetter)) {
+                vowelMap.set(lowerLetter, 1);
+            } else {
+                vowelMap.set(lowerLetter, vowelMap.get(lowerLetter) + 1);
+            }
+        }
+    }
+    return vowelMap;
+}
